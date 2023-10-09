4fba58caf1f774065f8eb4c0e029a464b82d10fb
VentoAureo230 <anthony.meny35@gmail.com>
2023-10-09 15:06:22 +0200

    🐛 FIX: Redirection homepage

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -13,23 +13,24 @@ class MyAppView extends StatelessWidget {
     return MaterialApp(
       title: 'Firebase Authentication',
       theme: ThemeData(
-          colorScheme: const ColorScheme.light(
-              background: Colors.white,
-              onBackground: Colors.black,
-              primary: Color.fromRGBO(206, 147, 216, 1),
-              onPrimary: Colors.black,
-              secondary: Color.fromRGBO(244, 143, 177, 1),
-              onSecondary: Colors.white,
-              tertiary: Color.fromRGBO(255, 204, 128, 1),
-              error: Colors.red,
-              outline: Color(0xFF424242))),
+        colorScheme: const ColorScheme.light(
+            background: Colors.white,
+            onBackground: Colors.black,
+            primary: Color.fromRGBO(206, 147, 216, 1),
+            onPrimary: Colors.black,
+            secondary: Color.fromRGBO(244, 143, 177, 1),
+            onSecondary: Colors.white,
+            tertiary: Color.fromRGBO(255, 204, 128, 1),
+            error: Colors.red,
+            outline: Color(0xFF424242)),
+      ),
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
-          if (state.status == AuthenticationState.authenticated) {
+          if (state.status == AuthenticationStatus.authenticated) {
             return BlocProvider(
               create: (context) => SignInBloc(
-                userRepository: context.read<AuthenticationBloc>().userRepository
-              ),
+                  userRepository:
+                      context.read<AuthenticationBloc>().userRepository),
               child: const HomeScreen(),
             );
           } else {
